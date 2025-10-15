619429bc59c66a4e01aa75f371b747077815f5fd
faisalamara411-cmd <faisalamara411@gmail.com>
2025-10-15 11:56:32 +0500

    Update frontend.py with new FastAPI URL and prediction fixes

--- src/frontend.py
+++ src/frontend.py
@@ -2,7 +2,8 @@ import streamlit as st
 import requests
 
 # URL of your FastAPI backend
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL= "http://127.0.0.1:8001/predict"
+
 
 st.title("IMDb Sentiment Analysis")
 
